a36f06416e8990bda600dde75fa718becb54b2b0
Garrett Moody <garrett.moody@snc.edu>
2018-04-08 16:14:28 -0500

    Create Public Menu created with simple navigation.
    Created the multiple menu screens with navigation through the screens.

---
+++ StreetTag/Assets/Scripts/Main Menu Scripts/StartGameCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenStartGameCanvas() {
+		this.gameObject.SetActive (true);
+	}
+
+	public void CloseStartGameCanvas() {
+		this.gameObject.SetActive (false);
+	}
+}
