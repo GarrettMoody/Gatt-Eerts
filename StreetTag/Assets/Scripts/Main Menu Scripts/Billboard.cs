5dabd170a4d1ad2195f8cc84ab3cd2badb4da78e
Garrett Moody <garrett.moody@snc.edu>
2018-05-07 21:28:06 -0500

    Main menu updates
    Continued adding functionality to the main menu. Used the standardized button and font.

---
+++ StreetTag/Assets/Scripts/Main Menu Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	public MainMenuCamera camera;
+	
+	public void OnButtonClickListener() {
+		camera.PanCameraToPlayMenu (1);
+	}
+}
