db25f84d58aeda81dff8b6fecd8500d9c5bfbbb9
Garrett Moody <garrett.moody@snc.edu>
2018-05-08 20:07:01 -0500

    Revert "Revert "Main menu updates""
    This reverts commit a1d2f0984a203cf14150b96e77bfc3d6f1bb00db.

---
+++ StreetTag/Assets/Scripts/Main Menu Scripts/Airplane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+	public float planeHeight;
+	public float leftMax;
+	public float rightMax;
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (.1f, .5f);
+		this.gameObject.transform.localPosition = new Vector3 (Random.Range(leftMax, rightMax), planeHeight, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPos = this.gameObject.transform.localPosition;
+		this.gameObject.transform.localPosition = new Vector3 (currentPos.x - speed, currentPos.y, currentPos.z);
+		if (this.gameObject.transform.localPosition.x <= leftMax) {
+			this.gameObject.transform.localPosition = new Vector3(rightMax, currentPos.y, currentPos.z);
+		}
+	}
+}
---
+++ StreetTag/Assets/Scripts/Main Menu Scripts/Bank.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour {
+
+	public MainMenuCamera camera;
+
+	public void OnButtonClickListener() {
+		camera.PanCameraToBankMenu (1);
+	}
+}
