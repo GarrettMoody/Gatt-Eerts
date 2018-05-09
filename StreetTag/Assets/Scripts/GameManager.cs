615046ba28963220d6e0e3fd7544e85b45b71752
Garrett Moody <garrett.moody@snc.edu>
2018-05-09 18:32:08 -0500

    Storm Marker resize on zoom
    The storm marker now resizes when player zooms. Now the storm marker appears to be in the same position at all times.

---
+++ StreetTag/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private int defaultZoom = 10;
+	private int stormMarkerIndex = 3;
+	private OnlineMapsMarker stormMarker;
+
+	void Start () {
+		OnlineMaps.instance.OnChangeZoom += OnChangeZoom;
+		stormMarker = OnlineMaps.instance.markers [stormMarkerIndex];
+	}
+
+	private void OnChangeZoom() {
+		int zoom = OnlineMaps.instance.zoom;
+		float originalScale = 1 << defaultZoom;
+		float currentScale = 1 << OnlineMaps.instance.zoom;
+		stormMarker.scale = currentScale / originalScale;
+
+		//playingFieldMarker.scale = currentScale / originalScale;
+	}
+}
