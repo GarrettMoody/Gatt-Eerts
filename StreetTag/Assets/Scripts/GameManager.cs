1c9bb565a75ce8f9b9837248f22c680d8391b67a
Garrett Moody <garrett.moody@snc.edu>
2018-06-24 15:25:06 -0500

    Revert "Change Play Game Menu"
    This reverts commit c67f47c76fe6e42d857e7d8c8d89d8bee11c7e58.

--- StreetTag/Assets/Scripts/GameManager.cs
+++ StreetTag/Assets/Scripts/GameManager.cs
@@ -6,27 +6,11 @@ public class GameManager : MonoBehaviour {
 
 	private int defaultZoom = 10;
 	private int stormMarkerIndex = 3;
-	private int mikeMarkerIndex = 2;
-	private int garrettMarkerIndex = 1;
-	private int jeffMarkerIndex = 0;
 	private OnlineMapsMarker stormMarker;
-	private OnlineMapsMarker garrettMarker;
-	private OnlineMapsMarker jeffMarker;
-	private OnlineMapsMarker mikeMarker;
-
 
 	void Start () {
 		OnlineMaps.instance.OnChangeZoom += OnChangeZoom;
 		stormMarker = OnlineMaps.instance.markers [stormMarkerIndex];
-		jeffMarker = OnlineMaps.instance.markers [jeffMarkerIndex];
-		mikeMarker = OnlineMaps.instance.markers [mikeMarkerIndex];
-		garrettMarker = OnlineMaps.instance.markers [garrettMarkerIndex];
-		
-	}
-
-	void Update() {
-		stormMarker.scale = stormMarker.scale - (stormMarker.scale * .001f);
-		OnlineMaps.instance.RedrawImmediately ();
 	}
 
 	private void OnChangeZoom() {
